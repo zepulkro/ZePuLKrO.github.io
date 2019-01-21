70fd01c478bd0b0ab05b4be60eea803d531e2d4a
Jorge Seminario <jorge.seminario.dev@gmail.com>
2019-01-21 14:55:02 -0500

    primer commit

---
+++ precache-manifest.eb98af7c167033a86d97a7f7fe29eff2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71952e8ed218bd3e7f37",
+    "url": "/static/css/main.47a8e23e.chunk.css"
+  },
+  {
+    "revision": "71952e8ed218bd3e7f37",
+    "url": "/static/js/main.71952e8e.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96d630d337d4419edafa28efd9f4755c",
+    "url": "/index.html"
+  }
+];
